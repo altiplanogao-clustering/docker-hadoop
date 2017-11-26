e17ec776910baa518684674d4ca2b452cd396ca7
Altiplano Gao <altiplanogao@hotmail.com>
2017-11-27 00:34:11 +0800

    Spark update

---
+++ spark/setup/set_spark_property.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function set_property(){
+	local prop=$1
+	kvp=(${prop//=/ })
+	name=${kvp[0]}
+	value=${kvp[1]}
+	conf_file=${SPARK_CONF_DIR}/spark-defaults.conf
+
+    echo "Setting SPARK property $name=$value"
+	sed -i 's|^$name.*$||g' ${conf_file}
+    echo $name $value >> ${conf_file}
+}
+
+function set_properties(){
+	local conf_str=$1
+	local conf_str_len=${#conf_str}
+	local first_letter=${conf_str:0:1}
+	if [[ ${conf_str:0:1} == '"' ]] ; then
+		((sub_len=$conf_str_len-2))
+		conf_str=${conf_str:1:$sub_len}
+	fi
+	if [ -n "$conf_str" ] ; then
+		echo "Processing configuration string: [$conf_str]"
+		for conf_item in ${conf_str[*]}
+		do
+			set_property $conf_item
+		done
+	fi
+}
--- spark/setup/update_spark_runtime_env.sh
+++ spark/setup/update_spark_runtime_env.sh
@@ -3,3 +3,9 @@
 
 sed -i 's|^export SPARK_DIST_CLASSPATH=.*$||g' ${SPARK_CONF_DIR}/spark-env.sh
 echo "export SPARK_DIST_CLASSPATH=$(${HADOOP_HOME}/bin/hadoop --config ${HADOOP_CONF_DIR} classpath)" >> ${SPARK_CONF_DIR}/spark-env.sh
+
+
+if [ -n "$SPARK_PROPERTY_CONF_DATA" ] ; then
+	source /docker-spark/setup/set_spark_property.sh
+	set_properties "$SPARK_PROPERTY_CONF_DATA"
+fi
