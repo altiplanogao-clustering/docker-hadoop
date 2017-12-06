ff9f23ed7ffd8f57fddbc7aa66133746efa32633
Altiplano <altiplanogao@hotmail.com>
2017-12-06 09:47:19 +0800

    Update PATH environment with HADOOP_HOME/bin

--- hadoop/setup/update_hadoop_env.sh
+++ hadoop/setup/update_hadoop_env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # env_files=( /etc/environment /etc/bash.bashrc /etc/profile )
+#env_files=( "/etc/environment" "/etc/bash.bashrc" "/etc/profile" )
 
-env_files=( "/etc/environment" "/etc/bash.bashrc" "/etc/profile" )
+profd_sh_file=/etc/profile.d/hadoop.sh
+touch $profd_sh_file
+env_files=( $profd_sh_file )
 for env_file in ${env_files[*]}
 do
 	exp="export "
@@ -13,6 +16,8 @@ do
 	echo "${exp}HADOOP_VERSION=${HADOOP_VERSION}" >> ${env_file}
 	echo "${exp}HADOOP_HOME=${install_path}" >> ${env_file}
 	echo "${exp}HADOOP_PREFIX=${install_path}" >> ${env_file}
+	echo "${exp}PATH=${install_path}/bin:\$PATH" >> ${env_file}
+
 	echo "${exp}HADOOP_HDFS_HOME=${install_path}" >> ${env_file}
 	echo "${exp}HADOOP_YARN_HOME=${install_path}" >> ${env_file}
 	echo "${exp}HADOOP_MAPRED_HOME=${install_path}" >> ${env_file}
@@ -23,5 +28,7 @@ do
 	echo "${exp}YARN_LOG_DIR=${hadoop_log_dir}" >> ${env_file}
 	echo "${exp}HADOOP_MAPRED_LOG_DIR=${hadoop_log_dir}" >> ${env_file}
 done
+
+
 
 
