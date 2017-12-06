b21bb62c37bdf4a36f06a258f517699fb59b9a28
Altiplano <altiplanogao@hotmail.com>
2017-12-06 09:46:16 +0800

    Update PATH environment

--- spark/setup/update_spark_env.sh
+++ spark/setup/update_spark_env.sh
@@ -2,7 +2,9 @@
 
 # env_files=( /etc/environment /etc/bash.bashrc /etc/profile )
 
-env_files=( "/etc/environment" "/etc/bash.bashrc" "/etc/profile" )
+profd_sh_file=/etc/profile.d/spark.sh
+touch $profd_sh_file
+env_files=( $profd_sh_file )
 for env_file in ${env_files[*]}
 do
 	exp="export "
@@ -13,5 +15,8 @@ do
 	echo "${exp}SPARK_HOME=${install_path}" >> ${env_file}
 	echo "${exp}SPARK_CONF_DIR=${spark_conf_dir}" >> ${env_file}
 	echo "${exp}SPARK_LOG_DIR=${spark_log_dir}" >> ${env_file}
+
+	echo "${exp}PATH=${install_path}/bin:\$PATH" >> ${env_file}
+
 done
 
